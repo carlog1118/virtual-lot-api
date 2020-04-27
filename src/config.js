4e4ecb9d4e4048f4b05dfd316987d5db254a50e2
Carlo Giustozzi <carlog1118@gmail.com>
2020-04-26 22:18:37 -0400

    deploy prep

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DB_URL || 'postgresql://virtual_lot@localhost/virtual-lot',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://virtual_lot@localhost/virtual-lot',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://virtual_lot@localhost/virtual-lot-test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 }
